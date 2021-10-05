44399d2f8b23d3d0a5fc9fcfdf33cfdf87f4b292
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-05 10:48:18 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,40 +1,93 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot start point """
-
+import glob
+import os
 import sys
-from importlib import import_module
+from pathlib import Path
 
-from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+import telethon.utils
+from telethon import TelegramClient
+from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
 
-from userbot import ALIVE_NAME, BOT_VER, LOGS, bot
-from userbot.modules import ALL_MODULES
+from userbot import ALIVE_NAME, APP_ID, API_HASH, BOT_TOKEN, BOT_USERNAME, BOT_VER, BOTLOG_CHATID, LOGS, UPSTREAM_REPO_BRANCH, bot, tgbot
+from userbot.modules.core import load_module
+HELL_PIC = ALIVE_LOGO or "https://telegra.ph/file/9dc4e335feaaf6a214818.jpg"
 
-INVALID_PH = (
-    "\nERROR: Nomor Telepon yang kamu masukkan SALAH."
-    "\nTips: Gunakan Kode Negara beserta nomornya atau periksa nomor telepon Anda dan coba lagi."
-)
 
-try:
-    bot.start()
-except PhoneNumberInvalidError:
-    print(INVALID_PH)
-    sys.exit(1)
+# let's get the bot ready
+async def manuserbot(bot_token):
+    try:
+        await bot.start(bot_token)
+        bot.me = await bot.get_me()
+        bot.uid = telethon.utils.get_peer_id(bot.me)
+    except Exception as e:
+        LOGS.error(f"STRING_SESSION - {str(e)}")
+        sys.exit()
 
-for module_name in ALL_MODULES:
-    imported_module = import_module("userbot.modules." + module_name)
 
+# Man-Userbot Starter
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    try:
+        if BOT_USERNAME is not None:
+            LOGS.info("Sedang Memeriksa BOT_USERNAME")
+            bot.tgbot = TelegramClient(
+                "BOT_TOKEN", api_id=APP_ID, api_hash=API_HASH
+            ).start(bot_token=BOT_TOKEN)
+            LOGS.info("Pemeriksaan Selesai. Melanjutkan ke Langkah berikutnya")
+            LOGS.info("🔰 Starting Man-UserBot 🔰")
+            bot.loop.run_until_complete(manuserbot(BOT_USERNAME))
+            LOGS.info("🔥 Man-UserBot Startup Completed 🔥")
+        else:
+            bot.start()
+    except Exception as e:
+        LOGS.error(f"BOT_TOKEN - {str(e)}")
+        sys.exit()
+
+
+# imports plugins...
+path = "userbot/modules/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
+
+
+# let the party begin...
+LOGS.info("Starting Bot Mode !")
+tgbot.start()
 LOGS.info(
     f"Jika {ALIVE_NAME} Membutuhkan Bantuan, Silahkan Gabung ke Grup https://t.me/SharingUserbot")
-
 LOGS.info(
     f"Man-Userbot ⚙️ V{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
 
 
+# that's life...
+async def man_is_on():
+    try:
+        if BOTLOG_CHATID != 0:
+            await bot.send_file(
+                BOTLOG_CHATID,
+                MAN_PIC,
+                caption=f"🔥 **Man-Userbot Berhasil Di Aktifkan**\n\n**➥ Userbot Version - {BOT_VER}@{UPSTREAM_REPO_BRANCH}**\n\n➥ **Ketik** `.ping` **atau** `.alive` **untuk Check BOT**\n➥ **Join @SharingUserbot Untuk Bantuan BOT**",
+            )
+    except Exception as e:
+        LOGS.info(str(e))
+
+
+# Join Channel after deploying 🤐😅
+    try:
+        await bot(JoinChannelRequest("@Lunatic0de"))
+    except BaseException:
+        pass
+
+
+bot.loop.create_task(man_is_on())
+
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
+    bot.tgbot = None
     bot.run_until_disconnected()
