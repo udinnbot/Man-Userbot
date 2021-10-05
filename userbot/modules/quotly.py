169e102b29e931a152fa9b4f6c5f4b3731bb2a1d
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-05 15:07:29 +0700

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -1,51 +1,45 @@
-from asyncio.exceptions import TimeoutError
-
+import asyncio
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from userbot import CMD_HELP, bot
 from userbot.events import register
+from userbot.utils import edit_delete, edit_or_reply
 
 
 @register(outgoing=True, pattern=r"^\.q")
-async def quotess(qotli):
-    if qotli.fwd_from:
+async def _(event):
+    if event.fwd_from:
         return
-    if not qotli.reply_to_msg_id:
-        return await qotli.edit("```Mohon Balas Ke Pesan```")
-    reply_message = await qotli.get_reply_message()
-    if not reply_message.text:
-        return await qotli.edit("```Mohon Balas Ke Pesan```")
+    if not event.reply_to_msg_id:
+        await edit_delete(event, "**Mohon Balas ke Pesan**", 15)
+        return
+    reply_message = await event.get_reply_message()
+    warna = event.pattern_match.group(1)
     chat = "@QuotLyBot"
-    if reply_message.sender.bot:
-        return await qotli.edit("```Mohon Balas Ke Pesan```")
-    await qotli.edit("```Sedang Memproses Sticker, Mohon Menunggu```")
-    try:
-        async with bot.conversation(chat) as conv:
-            try:
-                response = conv.wait_event(
-                    events.NewMessage(incoming=True, from_users=1031952739)
-                )
-                msg = await bot.forward_messages(chat, reply_message)
-                response = await response
-                """ - don't spam notif - """
-                await bot.send_read_acknowledge(conv.chat_id)
-            except YouBlockedUserError:
-                return await qotli.reply(
-                    "**Harap Jangan Blockir** @QuotLyBot **Buka Blokir Lalu Coba Lagi**"
-                )
-            if response.text.startswith("Hi!"):
-                await qotli.edit(
-                    "**Mohon Menonaktifkan Pengaturan Privasi Forward Anda**"
-                )
-            else:
-                await qotli.delete()
-                await bot.forward_messages(qotli.chat_id, response.message)
-                await bot.send_read_acknowledge(qotli.chat_id)
-                """ - cleanup chat after completed - """
-                await qotli.client.delete_messages(conv.chat_id, [msg.id, response.id])
-    except TimeoutError:
-        await qotli.edit()
+    await edit_or_reply(event, "`Processing...`")
+    async with bot.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1031952739)
+            )
+            first = await conv.send_message(f"/qcolor {warna}")
+            ok = await conv.get_response()
+            await asyncio.sleep(2)
+            second = await bot.forward_messages(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("**Silahkan Unblock @QuotLyBot dan coba lagi!!**")
+            return
+        if response.text.startswith("Hi!"):
+            await edit_or_reply(event, "**Mohon Menonaktifkan Pengaturan Privasi Forward Anda**"
+            )
+        else:
+            await event.delete()
+            await bot.forward_messages(event.chat_id, response.message)
+    await bot.delete_messages(
+        conv.chat_id, [first.id, ok.id, second.id, response.id]
+    )
 
 
 CMD_HELP.update(
