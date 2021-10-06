3cae4f49810b0dd5cec0a1c887938fe244041785
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-07 01:48:13 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -37,10 +37,10 @@ async def gcast(event):
         if x.is_group:
             chat = x.id
             try:
-                if chat not GCAST_BLACKLIST:
+                if chat not in GCAST_BLACKLIST:
                     await event.client.send_message(chat, msg)
                     done += 1
-                elif chat not GCAST_BLACKLIST:
+                elif chat not in GCAST_BLACKLIST:
                     pass
             except BaseException:
                 er += 1
