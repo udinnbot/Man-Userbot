7c942a0611e4608ebd0c637e0272fa6c41261ef3
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-06 23:19:31 +0700

    Update antiflood.py

--- userbot/modules/antiflood.py
+++ userbot/modules/antiflood.py
@@ -17,62 +17,53 @@ ANTI_FLOOD_WARN_MODE = ChatBannedRights(
 
 @register(incoming=True, disable_edited=True, disable_errors=True)
 async def _(event):
-    # logger.info(CHAT_FLOOD)
     if not CHAT_FLOOD:
         return
-    admin_c = await is_admin(event.chat_id, event.message.from_id)
-    if admin_c:
+    admin_c = await is_admin(event.client, event.chat_id, event.client.uid)
+    if not admin_c:
         return
     if str(event.chat_id) not in CHAT_FLOOD:
         return
-    should_ban = sql.update_flood(event.chat_id, event.message.from_id)
+    should_ban = sql.update_flood(event.chat_id, event.message.sender_id)
     if not should_ban:
         return
     try:
         await event.client(
             EditBannedRequest(
-                event.chat_id, event.message.from_id, ANTI_FLOOD_WARN_MODE
+                event.chat_id, event.message.sender_id, ANTI_FLOOD_WARN_MODE
             )
         )
-    except Exception as e:  # pylint:disable=C0103,W0703
+    except Exception as e:
         no_admin_privilege_message = await event.client.send_message(
             entity=event.chat_id,
-            message="""**Automatic AntiFlooder**
-[User](tg://user?id={}) Membanjiri obrolan.
-
-`{}`""".format(
-                event.message.from_id, str(e)
-            ),
+            message=f"**Automatic AntiFlooder**\x1f[Jamet](tg://user?id={event.message.sender_id}) Membanjiri obrolan.\x1f`{e}`",
             reply_to=event.message.id,
         )
-        await asyncio.sleep(10)
-        await no_admin_privilege_message.edit("Sadly u don't have admin privilege")
+
+        await asyncio.sleep(4)
+        await no_admin_privilege_message.edit(
+            "**Ini SPAM tidak berguna kawan. Hentikan ini, Mari kita parming**"
+        )
     else:
         await event.client.send_message(
             entity=event.chat_id,
-            message="""**Automatic AntiFlooder**
-[User](tg://user?id={}) Membanjiri obrolan.
-**Aksi:** Saya membisukan dia 🔇""".format(
-                event.message.from_id
-            ),
+            message=f"""**Automatic AntiFlooder**
+[User](tg://user?id={event.message.sender_id}) Membanjiri obrolan.
+**Aksi:** Saya membisukan dia""",
             reply_to=event.message.id,
         )
 
 
 @register(outgoing=True, pattern=r"^\.setflood(?: |$)(.*)")
 async def _(event):
-    if event.fwd_from:
-        return
     input_str = event.pattern_match.group(1)
+    event = await event.edit("`Processing...`")
+    await asyncio.sleep(2)
     try:
         sql.set_flood(event.chat_id, input_str)
         sql.__load_flood_settings()
-        await event.edit(
-            "**Antiflood diperbarui menjadi** `{}` **dalam obrolan saat ini**".format(
-                input_str
-            )
-        )
-    except Exception as e:  # pylint:disable=C0103,W0703
+        await event.edit(f"**Antiflood diperbarui menjadi** `{input_str}` **dalam obrolan saat ini**")
+    except Exception as e:
         await event.edit(str(e))
 
 
