d5bcf1a88984b9d0ad6dc518bc024c873acd8dc3
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-09 23:33:15 +0700

    Update evaluators.py

--- userbot/modules/evaluators.py
+++ userbot/modules/evaluators.py
@@ -17,6 +17,8 @@ from userbot.utils import edit_delete, edit_or_reply
 
 MAX_MESSAGE_SIZE_LIMIT = 4095
 
+p = print
+
 
 @register(outgoing=True, pattern=r"^\.eval(?: |$|\n)([\s\S]*)")
 async def _(event):
