50cd2911bdd12f6791630f6245e02e8b718847a8
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-09 12:11:44 +0700

    Update tag_all.py

--- userbot/modules/tag_all.py
+++ userbot/modules/tag_all.py
@@ -3,14 +3,20 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """A Plugin to tagall in the chat for @UniBorg and cmd is `.all`"""
 
+import asyncio
+import random
 import re
 
+from time import sleep
 from userbot import CMD_HELP, bot
 from userbot.events import register
 
 usernexp = re.compile(r"@(\w{3,32})\[(.+?)\]")
 nameexp = re.compile(r"\[([\w\S]+)\]\(tg://user\?id=(\d+)\)\[(.+?)\]")
+emoji = "🐵 🦁 🐯 🐱 🐶 🐺 🐻 🐨 🐼 🐹 🐭 🐰 🦊 🦝 🐮 🐷 🐽 🐗 🦓 🦄 🐴 🐸 🐲 🦎 🐉 🦖 🦕 🐢 🐊 🐍 🐁 🐀 🐇 🐈 🐩 🐕 🦮 🐕‍🦺 🐅 🐆 🐎 🐖 🐄 🐂 🐃 🐏 🐑 🐐 🦌 🦙 🦥 🦘 🐘 🦏 🦛 🦒 🐒 🦍 🦧 🐪 🐫 🐿️ 🦨 🦡 🦔 🦦 🦇 🐓 🐔 🐣 🐤 🐥 🐦 🦉 🦅 🦜 🕊️ 🦢 🦩 🦚 🦃 🦆 🐧🦈 🐬 🐋 🐳 🐟 🐠 🐡 🦐 🦞 🦀 🦑 🐙 🦪 🦂 🕷️ 🦋 🐞 🐝 🦟 🦗 🐜 🐌 🐚 🕸️ 🐛 🐾 😀 😃 😄 😁 😆 😅 😂 🤣 😭 😗 😙 😚 😘 🥰 😍 🤩 🥳 🤗 🙃 🙂 ☺️ 😊 😏 😌 😉 🤭 😶 😐 😑 😔 😋 😛 😝 😜 🤪 🤔 🤨 🧐 🙄 😒 😤 😠 🤬 ☹️ 🙁 😕 😟 🥺 😳 😬 🤐 🤫 😰 😨 😧 😦 😮 😯 😲 😱 🤯 😢 😥 😓 😞 😖 😣 😩 😫 🤤 🥱 😴 😪 🌛 🌜 🌚 🌝 🌞 🤢 🤮 🤧 🤒 🍓 🍒 🍎 🍉 🍑 🍊 🥭 🍍 🍌 🌶 🍇 🥝 🍐 🍏 🍈 🍋 🍄 🥕 🍠 🧅 🌽 🥦 🥒 🥬 🥑 🥯 🥖 🥐 🍞 🥜 🌰 🥔 🧄 🍆 🧇 🥞 🥚 🧀 🥓 🥩 🍗 🍖 🥙 🌯 🌮 🍕 🍟 🥨 🥪 🌭 🍔 🧆 🥘 🍝 🥫 🥣 🥗 🍲 🍛 🍜 🍢 🥟 🍱 🍚 🥡 🍤 🍣 🦞 🦪 🍘 🍡 🥠 🥮 🍧 🍧 🍨".split(" ")
 
+class FlagContainer:
+    is_active = False
 
 @register(outgoing=True, pattern=r"^\.all(?: |$)(.*)", disable_errors=True)
 async def all(event):
@@ -25,11 +31,61 @@ async def all(event):
     await bot.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
 
 
+@register(outgoing=True, groups_only=True, pattern=r"^\.emojitag(?: |$)(.*)")
+async def b(event):
+    if event.fwd_from or FlagContainer.is_active:
+        return
+    try:
+        FlagContainer.is_active = True
+
+        text = None
+        args = event.message.text.split(" ", 1)
+        if len(args) > 1:
+            text = args[1]
+
+        chat = await event.get_input_chat()
+        await event.delete()
+
+        tags = list(map(lambda m: f"[{random.choice(emoji)}](tg://user?id={m.id})", await event.client.get_participants(chat),
+		),
+		   )
+        current_pack = []
+        async for participant in event.client.iter_participants(chat):
+            if not FlagContainer.is_active:
+                break
+
+            current_pack.append(participant)
+
+            if len(current_pack) == 5: 
+                tags = list(map(lambda m: f"[{random.choice(emoji)}](tg://user?id={m.id})", current_pack
+			      ),
+			   )
+                current_pack = []
+
+                if text:
+                    tags.append(text)
+
+                await event.client.send_message(event.chat_id, " ".join(tags))
+                await asyncio.sleep(1.3) 
+    finally:
+        FlagContainer.is_active = False
+
+
 CMD_HELP.update(
     {
         "tag_all": "**Plugin : **`tag_all`\
         \n\n  •  **Syntax :** `.all`\
         \n  •  **Function : **Untuk Mengetag semua anggota yang ada di group.\
+    "
+    }
+)
+
+
+CMD_HELP.update(
+    {
+        "emojitag": "**Plugin : **`emojitag`\
+        \n\n  •  **Syntax :** `.emojitag`\
+        \n  •  **Function : **Untuk Mengetag semua anggota di grup dengan emoji berbeda..\
     "
     }
 )
